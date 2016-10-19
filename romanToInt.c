6eb5fea06447c9d1f7ff0648eebbac9c2120f24f
LyleMi <1278919492@qq.com>
2016-10-19 08:46:21 +0800

    add

--- romanToInt.c
+++ romanToInt.c
@@ -2,10 +2,9 @@ int romanToInt(char* s) {
     int sum = 0, curr = 0, pre = 0;
     int roman[] = {100, 500, 0, 0, 0, 0, 1, 0, 0, 50, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 10};
     while (*s) {
-        curr = roman[*s - 'C'];
-        sum += curr - (pre < curr) * ( pre << 1 );
+        curr = roman[*s++ -'C'];
+        sum += curr - ( pre < curr ) * ( pre << 1 );
         pre = curr;
-        s++;
     }
     return sum;
 }
