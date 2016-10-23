937ed1069fa1a46e56b684d53bd8bd633c0ee527
LyleMi <1278919492@qq.com>
2016-10-23 09:59:08 +0800

    add comment

--- originalDigits.py
+++ originalDigits.py
@@ -10,3 +10,38 @@ class Solution(object):
         c[9] -= (c[5] + c[6] + c[8])
 
         return ''.join(map(lambda i: str(i) * c[i], range(10)))
+
+'''
+
+
+Given a non-empty string containing an out-of-order English representation of digits 0-9, output the digits in ascending order.
+
+Note:
+
+    Input contains only lowercase English letters.
+    Input is guaranteed to be valid and can be transformed to its original digits. That means invalid inputs such as "abc" or "zerone" are not permitted.
+    Input length is less than 50,000.
+
+Example 1:
+
+Input: "owoztneoer"
+
+Output: "012"
+
+Example 2:
+
+Input: "fviefuro"
+
+Output: "45"
+
+'''
+
+'''
+
+0, 1, 2,...
+
+some of them has unique str
+such as 'z' in 'zero'
+and could calc it
+
+'''
